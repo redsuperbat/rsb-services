c02a511e76faa808ab38b49a37c2f546fd1e4689
Max Netterberg <max.netterberg@gmail.com>
2021-11-14 18:57:14 +0100

    antoher small fix

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Plex
 REPO_PATH=~/Versioned/rsb-services
 
-# if [ ! /plex ]; then
-#   sudo mkdir -d /plex
+# if [ ! -d /plex ]; then
+#   sudo mkdir /plex
 # fi
 
 # if [ ! -d /plex/movies ]; then
@@ -17,10 +17,10 @@ REPO_PATH=~/Versioned/rsb-services
 #   sudo mkdir /plex/config
 # fi
 
-docker-compose up -f $REPO_PATH/plex/docker-compose.yaml -d
+docker-compose up -d -f $REPO_PATH/plex/docker-compose.yaml
 
 # Portainer
-docker-compose up -f $REPO_PATH/portainer/docker-compose.yaml -d
+docker-compose up -d -f $REPO_PATH/portainer/docker-compose.yaml
 
 # Nextcloud
-docker-compose up -f $REPO_PATH/nextcloud/docker-compose.yaml -d
+docker-compose up -d -f $REPO_PATH/nextcloud/docker-compose.yaml
