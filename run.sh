0b01fc55a1b22b834216960ce87af0a08bc11351
Max Netterberg <44667637+redsuperbat@users.noreply.github.com>
2022-01-05 20:59:39 +0100

    added photo prism compose file

--- run.sh
+++ run.sh
@@ -1,27 +1,12 @@
 # Plex
 REPO_PATH=~/Versioned/rsb-services
-
-# if [ ! -d /plex ]; then
-#   sudo mkdir /plex
-# fi
-
-# if [ ! -d /plex/movies ]; then
-#   sudo mkdir /plex/movies
-# fi
-
-# if [ ! -d /plex/tv ]; then
-#   sudo mkdir /plex/tv
-# fi
-
-# if [ ! -d /plex/config ]; then
-#   sudo mkdir /plex/config
-# fi
-
 docker-compose -f $REPO_PATH/plex/docker-compose.yaml up -d
 
 # Portainer
 docker-compose -f $REPO_PATH/portainer/docker-compose.yaml up -d
 
+# Nextcloud
+## Promp user for passwords and usernames for the services
 if [ ! -f $REPO_PATH/nextcloud/postgres.env ] || [ ! -f $REPO_PATH/nextcloud/nextcloud.env ]; then
   echo "Postgres db username: "
   read PG_USER
@@ -37,6 +22,5 @@ if [ ! -f $REPO_PATH/nextcloud/postgres.env ] || [ ! -f $REPO_PATH/nextcloud/nex
   echo "POSTGRES_HOST=postgres" >>$REPO_PATH/nextcloud/nextcloud.env
   echo "POSTGRES_DB=nextcloud" >>$REPO_PATH/nextcloud/nextcloud.env
 fi
-
-# Nextcloud
+## Run the nextcloud container
 docker-compose -f $REPO_PATH/nextcloud/docker-compose.yaml up -d
